15e6bf31b495d0d5f658ccc9d27b688fbb51c67c
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-01-28 21:41:51 -0500

    Declared a new subclass of Enemy for an alien.

---
+++ GalaxyWars/Alien.swift
@@ -0,0 +1,15 @@
+//
+//  Alien.swift
+//  GalaxyWars
+//
+//  Created by Emanuel  Guerrero on 1/28/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+
+class Alien: Enemy {
+    override var type: String {
+        return "Alien"
+    }
+}
